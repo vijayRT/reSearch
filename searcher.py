16ef252bf446e3c6053584e7cd2e538aef49d66f
vijayRT <vijayrajasekar1@gmail.com>
2018-09-24 17:58:43 -0400

    Minor changes to searcher

--- searcher.py
+++ searcher.py
@@ -1,10 +1,10 @@
-from whoosh.index import create_in, open_dir
+from whoosh.index import open_dir
 from whoosh.fields import *
 
 from whoosh.qparser import QueryParser
 
 ix = open_dir("./indexdir")
-queryString = input("Enter a query string")
+queryString = input("Enter a query string: \n")
 with ix.searcher() as searcher:
     query = QueryParser("content", schema=ix.schema).parse(queryString)
     results = searcher.search(query)
