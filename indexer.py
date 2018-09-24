98264753fba254ebae9e0d599aa50de0dbd571f5
vijayRT <vijayrajasekar1@gmail.com>
2018-09-24 15:36:59 -0400

    Added file title display

--- indexer.py
+++ indexer.py
@@ -2,23 +2,20 @@ import os, os.path
 from whoosh.index import create_in
 from whoosh.fields import *
 
-schema = Schema(path=ID(stored=True), content=TEXT)
+schema = Schema(path=ID(stored=True), title=TEXT(stored=True), content=TEXT)
 if not os.path.exists("indexdir"):
     os.mkdir("indexdir")
 
 ix = create_in("indexdir", schema)
-
 writer = ix.writer()
+
 for path, directories, files in os.walk('.\data'):
     for file in files:
         filepath = os.path.join(path, file)
         with open(filepath, "r", encoding='utf-8') as f:
-            writer.add_document(path=filepath, content=f.read())
+            content = f.read()
+            title = content.split("\n")[0]
+            writer.add_document(path=filepath, title=title, content=content)
             print("Indexed filename: ", filepath)
+            print(title)
 writer.commit()
-
-from whoosh.qparser import QueryParser
-with ix.searcher() as searcher:
-    query = QueryParser("content", ix.schema).parse("sequential")
-    results = searcher.search(query)
-    print(results)
--- searcher.py
+++ searcher.py
@@ -4,8 +4,9 @@ from whoosh.fields import *
 from whoosh.qparser import QueryParser
 
 ix = open_dir("./indexdir")
+queryString = input("Enter a query string")
 with ix.searcher() as searcher:
-    query = QueryParser("content", schema=ix.schema).parse("first")
+    query = QueryParser("content", schema=ix.schema).parse(queryString)
     results = searcher.search(query)
     for result in results:
-        print(result)
+        print(result['title'])
